fff23d78f564349a4f5834fa850bbf71a6e6e091
Mars <johny.black.1991@gmail.com>
2020-06-04 23:01:55 +0300

    Начало работы

---
+++ lab04-2/histogram.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void find_minmax(const vector<double>& numbers, double& min, double& max);
+vector<double> input_numbers(size_t count);
+vector <size_t> make_histogram(const vector<double>& numbers, size_t bin_count, double min, double max);
+void show_histogram_text(vector <size_t> bins);
---
+++ lab04-2/main.cpp
@@ -0,0 +1,27 @@
+#include "histogram.h"
+#include "svg.h"
+using namespace std;
+
+int main()
+{
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+
+    cerr << "Enter numbers: ";
+    const auto numbers = input_numbers(number_count);
+
+    size_t bin_count;
+    cerr << "Enter column count: ";
+    cin >> bin_count;
+
+    double min;
+    double max;
+    find_minmax(numbers,min,max);
+
+    auto bins = make_histogram(numbers, bin_count, min, max);
+
+    show_histogram_svg(bins, bin_count);
+
+    return 0;
+}
