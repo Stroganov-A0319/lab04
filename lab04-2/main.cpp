407ab57881f27371a8ac6c4455fc459e3526e6ab
Mars <johny.black.1991@gmail.com>
2020-06-05 20:23:37 +0300

    Форматный вывод в стиле С

--- lab04-2/main.cpp
+++ lab04-2/main.cpp
@@ -1,9 +1,18 @@
 #include "histogram.h"
-#include "svg.h"
+#include "SVG.h"
 using namespace std;
 
 int main()
 {
+    const char* name = "Commander Shepard";
+    int year = 2154;
+    printf("%s was born in %d.\n", name, year);
+    // Commander Shepard was born in 2154.
+
+    printf("n = %08x\n", 0x1234567); // 01234567
+
+    return 0;
+
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
