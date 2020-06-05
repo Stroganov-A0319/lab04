eb0fb7350996f277a6c10d31dbbdfe85d385d016
Mars <johny.black.1991@gmail.com>
2020-06-05 17:19:54 +0300

    Добавил read_input() новый параметр, который определяет необходимость вывода в cerr подсказок

--- lab04-2/main.cpp
+++ lab04-2/main.cpp
@@ -3,24 +3,34 @@
 #include <iostream>
 using namespace std;
 
-Input read_input(istream& in) {
+Input read_input(istream& in, bool prompt) {
     Input data;
-    cerr << "Enter number count: ";
+
+    if(prompt)
+    {
+        cerr << "Enter number count: ";
+    }
     size_t number_count;
     in >> number_count;
 
-    cerr << "Enter numbers: ";
+    if(prompt)
+    {
+        cerr << "Enter numbers: ";
+    }
     data.numbers = input_numbers(in, number_count);
 
-    cerr << "Enter column count: ";
+    if(prompt)
+    {
+        cerr << "Enter column count: ";
+    }
     in >> data.bin_count;
 
     return data;
 }
 int main()
 {
-    Input input = read_input(cin);
-    const auto bins = make_histogram(input);
+    Input input = read_input(cin, true);
+    vector<size_t> bins = make_histogram(input);
     show_histogram_svg(bins, input.bin_count);
     return 0;
 }
